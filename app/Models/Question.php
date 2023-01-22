b71a18365d1e03ee2ccceaadd9f026c0eb17d036
Abdoulie Nyass <nyassabu@gmail.com>
2023-01-22 14:10:12 +0000

    add fillable and scopeFilter to question model

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['question', 'anonymous', 'tags', 'user_id', 'created_at', 'updated_at'];
+
+    public function scopeFilter($query, array $filters)
+    {
+        if (isset($filters['tag']))
+            $query->where('tags', 'like', '%' . $filters['tag'] . '%');
+        elseif (isset($filters['search']))
+            $query->where('question', 'like', '%' . $filters['search'] . '%')
+                ->orWhere('tags', 'like', '%' . $filters['search'] . '%');
+    }
 }
