0b2974f478c5bbbcc4c71d7c280d8d01b5f07a5c
Abdoulie Nyass <nyassabu@gmail.com>
2023-01-22 14:20:24 +0000

    add questions.index view

---
+++ resources/views/questions/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layout')
+
+@section('head')
+    <title>QA: Home</title>
+@endsection
+
+
+@section('content')
+    <div class="gap-3 d-flex flex-column">
+        @unless(count($questions) == 0)
+            @foreach ($questions as $question)
+                <x-question-card :question="$question" />
+            @endforeach
+        @else
+            <p class='lead'>Hmm, sad, very sad, no questions?</p>
+        @endunless
+    </div>
+@endsection
