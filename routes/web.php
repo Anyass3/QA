867bebe2430f704faef383b725498fb060bcf4a0
Abdoulie Nyass <nyassabu@gmail.com>
2023-01-22 14:24:33 +0000

    update web routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\QuestionController;
+use App\Models\Question;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
+Route::get('/', [QuestionController::class, 'index']);
+Route::post('/questions', [QuestionController::class, 'store']);
+Route::get('/questions/create', [QuestionController::class, 'create']);
+Route::get('/questions/{question}', [QuestionController::class, 'show']);
+
+Route::get('/hello-world', function () {
+    return response('<p>my <i>Zainab Nyass</i></p>', 200)
+        ->header('Content-Type', 'text/plain');
+});
+Route::get('/slug/{name}', function ($name, Request $req) {
+    return str()->slug($name . ' ' . fake('en')->sentence());
+})->where('name', '[0-9]+');
+
+Route::get('/search', function (Request $req) {
+    dd($req);
 });
